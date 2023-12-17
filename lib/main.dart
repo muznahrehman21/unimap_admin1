185ca1517b77efa3757ba761cf1f3131e66dfc0e
Asad ur Rehman Khan <asaddigital@gmail.com>
2023-12-17 17:28:17 +0500

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:unimap_admin/firebase_options.dart';
 import 'package:unimap_admin/views/splash/splashScreen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
